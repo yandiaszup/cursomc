8b3311f54e90e61202766b679ea00bea66a604c0
yandiaszup <yan.dias@zup.com.br>
2018-08-23 17:05:36 -0300

    Atualizando Categoria com PUT

--- src/main/java/com/example/cursomc/services/ClienteServices.java
+++ src/main/java/com/example/cursomc/services/ClienteServices.java
@@ -17,7 +17,7 @@ public class ClienteServices {
     private ClienteRepository repo;
 
 
-    public Cliente buscar(Integer id) {
+    public Cliente find(Integer id) {
         Optional<Cliente> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
